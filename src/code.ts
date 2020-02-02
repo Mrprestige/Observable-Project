91b4a0c7d0fbd06951a7c742ebd5d62f7839f8a9
mohsen mohammadi rahnama <mrprestige344@gmail.com>
2020-02-02 14:09:19 +0330

    Hot-vs-cold

--- src/code.ts
+++ src/code.ts
@@ -1,3 +1,29 @@
-import * as Rx from "rxjs/Observable";
-
-console.log(Rx);
+import { Observable } from "rxjs/Observable";
+import "rxjs/add/operator/share";
+var observable = Observable.create((observer: any) => {
+  try {
+    observer.next("Hi Mohsen :)");
+    observer.next("How are you? :)");
+    setInterval(() => {
+      observer.next("ممنون عزیزم");
+    }, 2000);
+  } catch (error) {
+    observer.error(error);
+  }
+}).share();
+var observer = observable.subscribe(
+  (x: any) => addItem(x),
+  (err: any) => addItem(err),
+  () => addItem("Completed!")
+);
+setTimeout(() => {
+  var observer2 = observable.subscribe((x: any) =>
+    addItem("Subscriber 2: " + x)
+  );
+}, 1000);
+function addItem(val: any) {
+  var node = document.createElement("li");
+  var textnode = document.createTextNode(val);
+  node.appendChild(textnode);
+  document.getElementById("output").appendChild(node);
+}
