40b3029fde20d4ed1cbaa057924e9edd33ad7f47
Mohsen <mrprestige344@gmail.com>
2020-02-12 19:58:25 +0330

    rename

--- src/index.ts
+++ src/index.ts
@@ -27,6 +27,7 @@ let observable = fromEvent(document, "click");
 let subscription = observable.subscribe((x: any) => {
   addItem(x)
 })
+
 function addItem(val: any) {
   var node = document.createElement("li");
   var textnode = document.createTextNode(val);
