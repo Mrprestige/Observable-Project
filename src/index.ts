486dee88be3562fe530128a9a9a1c1080af4ec98
Mohsen <mrprestige344@gmail.com>
2020-02-20 21:07:18 +0330

    Relearn

--- src/index.ts
+++ src/index.ts
@@ -22,15 +22,36 @@
 //     addItem("Subscriber 2: " + x)
 //   );
 // }, 1000);
-import { fromEvent } from "rxjs/Observable/fromEvent"
-let observable = fromEvent(document, "click");
-let subscription = observable.subscribe((x: any) => {
-  addItem(x)
-})
 
-function addItem(val: any) {
-  var node = document.createElement("li");
-  var textnode = document.createTextNode(val);
-  node.appendChild(textnode);
-  document.getElementById("output").appendChild(node);
+// import { fromEvent } from "rxjs/Observable/fromEvent"
+// let observable = fromEvent(document, "click");
+// let subscription = observable.subscribe((x: any) => {
+//   addItem(x)
+// })
+
+// function addItem(val: any) {
+//   var node = document.createElement("li");
+//   var textnode = document.createTextNode(val);
+//   node.appendChild(textnode);
+//   document.getElementById("output").appendChild(node);
+// }
+
+import { Observable } from "rxjs";
+
+var observable = Observable.create((obsever: any) => {
+  obsever.next('Hi Mohsen');
+  obsever.next('How are you bro!');
+  obsever.complete();
+});
+observable.subscribe(
+  (showValue: any) => listItem(showValue),
+  (err: any) => console.log(err),
+  () => listItem('its finished !')
+);
+
+function listItem(val: any) {
+  var node = document.createElement('li');
+  var textNode = document.createTextNode(val);
+  node.appendChild(textNode);
+  document.getElementById('output').appendChild(node);
 }
